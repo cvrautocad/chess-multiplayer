e8a32a1db0c5039d756ef276ffe56dfddfa25773
cvrautocad <cvrautocad@gmail.com>
2025-04-06 16:39:58 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  matchesPlayed: { type: Number, default: 0 },
+  matchesWon: { type: Number, default: 0 },
+  matchesLost: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("User", UserSchema);
